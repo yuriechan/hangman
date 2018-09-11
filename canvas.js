2538bbda4acb6a7c56920a7c2c369f995b031fbf
Yurie <cutecupcakeandcute@gmail.com>
2018-09-11 14:23:10 +0900

    second edit to hangman design

--- canvas.js
+++ canvas.js
@@ -42,4 +42,53 @@ context.lineTo(120, 190);
 context.moveTo(197, 180);
 context.stroke();
 
-//*** right
+//*** right leg of hangman**//
+context.beginPath();
+context.lineWidth = 8;
+context.strokeStyle = 'yellow';
+
+context.moveTo(203, 297);
+context.lineTo(280, 310);
+context.moveTo(203, 297);
+context.stroke();
+
+//***left leg of hangman**//
+context.beginPath();
+context.lineWidth = 8;
+context.strokeStyle = 'pink';
+
+context.moveTo(197, 297);
+context.lineTo(120, 307);
+context.moveTo(197, 297);
+context.stroke();
+
+//*** the top part of hang**//
+context.beginPath();
+context.lineWidth = 15;
+context.strokeStyle = 'black';
+
+context.moveTo(200, 0);
+context.lineTo(200, 47);
+context.moveTo(200, 0);
+context.stroke();
+
+//*** top extension part of hang**//
+context.beginPath();
+context.lineWidth = 15;
+context.strokeStyle = 'black';
+
+context.moveTo(200, 0);
+context.lineTo(30, 0);
+context.moveTo(200, 0);
+context.stroke();
+
+//*** other parts of the hang**//
+context.beginPath();
+context.lineWidth = 15;
+context.strokeStyle = 'black';
+
+context.moveTo(30, 0);
+context.lineTo(30, 450);
+context.lineTo(350, 450);
+context.moveTo(30, 0);
+context.stroke();
